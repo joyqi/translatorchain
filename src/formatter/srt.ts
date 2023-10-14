ad4f4a8933260de83f2636ee89752dc1a1269277
joyqi <magike.net@gmail.com>
2023-10-14 21:22:58 +0800

    fix srt format

--- src/formatter/srt.ts
+++ src/formatter/srt.ts
@@ -5,12 +5,15 @@ export default class implements Formatter {
         const lines = text.split(/\r?\n/).filter(line => line.trim().length > 0);
         const data: Record<string, string> = {};
         let key = '';
+        let seq = 0;
 
         lines.forEach((line, i) => {
-            if (i % 2 === 0) {
+            if (i % 3 === 0) {
+                seq = parseInt(line);
+            } else if (i % 3 === 1) {
                 key = line;
             } else {
-                data[key] = line;
+                data[seq + '|' + key] = line;
             }
         });
 
@@ -18,7 +21,11 @@ export default class implements Formatter {
     }
 
     marshal(data: any, indent: number): string {
-        const lines = Object.entries(data).map(([key, value]) => `${key}\n${value}`);
+        const lines = Object.entries(data).map(([key, value]) => {
+            const [seq, k] = key.split('|');
+            return `${seq}\n${k}\n${value}`;
+        });
+
         return lines.join('\n\n');
     }
 }
